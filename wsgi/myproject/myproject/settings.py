4910e7ecc1b1d3318922c8034b1ec977d482080d
ChenLiang <236338364@qq.com>
2016-05-09 00:19:21 +0800

    remove static

--- wsgi/myproject/myproject/settings.py
+++ wsgi/myproject/myproject/settings.py
@@ -137,17 +137,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(WSGI_DIR, 'static')
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = [
-os.path.join(BASE_DIR, 'templates'),
-]
+# TEMPLATE_DIRS = [
+# os.path.join(BASE_DIR, 'templates'),
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+# STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
-STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#  os.path.join(BASE_DIR, 'static'),
+# )
