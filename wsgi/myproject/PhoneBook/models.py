e64fe6fcdcc9fe07c35af93ff5480a5604136051
ChenLiang <236338364@qq.com>
2016-05-21 14:52:08 +0800

    ...

--- wsgi/myproject/PhoneBook/models.py
+++ wsgi/myproject/PhoneBook/models.py
@@ -7,11 +7,6 @@ from django.core.urlresolvers import reverse
 class MessageUser(models.Model):
 	Name=models.CharField(max_length=100) #姓名
 	
-	# PhoneNum=models.IntegerField() #电话
-	# phone_regex=RegexValidator(regex=r'^\d{11}$',message='''Phone number 
-	# 	must be entered in the format: 13588888888. Up to 11 digits allowed. ''')
-	# PhoneNum=models.CharField(validators=[phone_regex],blank=True,max_length=11)
-
 	Address=models.TextField(blank=True)		#住址
 	Other=models.TextField(blank=True)    #其他
 	UpdateTime=models.DateTimeField('更新时间',auto_now=True,blank=True) #时间1
